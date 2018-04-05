ecedd374b98806ec8d0a630f1691c17c724504c9
nworbnitsuj <justin.v.brown@gmail.com>
2018-04-05 16:10:10 -0500

    Added event listeners to each button that console log the click event

---
+++ app.js
@@ -0,0 +1,23 @@
+/// Button 1 listener ///
+let button1 = document.getElementById('button1');
+button1.addEventListener("click", (event) => {
+  console.log("Button 1 clicked");
+})
+
+/// Button 2 listener ///
+let button2 = document.getElementById('button2');
+button2.addEventListener("click", (event) => {
+  console.log("Button 2 clicked");
+})
+
+/// Button 3 listener ///
+let button3 = document.getElementById('button3');
+button3.addEventListener("click", (event) => {
+  console.log("Button 3 clicked");
+})
+
+/// Button 4 listener ///
+let button4 = document.getElementById('button4');
+button4.addEventListener("click", (event) => {
+  console.log("Button 4 clicked");
+})
